28284c8f901159ccb1dc11e1af543d39852de1a6
Thomas Dippel <dipth@mjolnirm4.local>
2025-01-20 21:34:36 +0100

    set a default mail from address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: Rails.application.credentials.dig(:postmark, :from_email)
+  default from: (Rails.application.credentials.dig(:postmark, :from_email) || "from@example.com")
   layout "mailer"
 end
